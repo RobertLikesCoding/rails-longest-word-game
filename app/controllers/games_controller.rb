87a7bc2f7713629702d915613c205a19d1b61c6d
Robert Baufeld <rbaufeld@posteo.de>
2024-05-13 14:49:25 +0200

    main part done, optionals still open

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,6 @@
 require 'open-uri'
-require 'json'
 
 class GamesController < ApplicationController
-  # @word =
-
   def new
     alphabet = ("A".."Z").to_a
     @letters = 10.times.map { alphabet.sample }
@@ -12,12 +9,11 @@ class GamesController < ApplicationController
   def score
     @letters = params[:letters]
     @user_input = params[:word].upcase
-    letters_exist = @user_input.upcase.split(//).all? { |letter| @letters.include?(letter) }
 
-    if letters_exist
-      uri_response = URI.open("https://dictionary.lewagon.com/#{@user_input}").read
-      parsed_response = JSON.parse(uri_response)
-      @word_found = parsed_response[:found]
+    if word_in_grid?(@user_input, @letters) && user_input.length < 10 && english_word?(@user_input)
+      @result = "<p><strong>Congratulations!</strong> #{@user_input} is a valid word!</p>"
+    else
+      @result = "<p>Sorry but <strong>#{@user_input}</strong> can't be built out of #{@letters.join(', ')}</p>"
     end
 
   end
@@ -28,19 +24,9 @@ class GamesController < ApplicationController
     user_input.chars.all? { |char| letters.include?(char) }
   end
 
-  def char_usage_ok?(attempt, grid)
-    parameter = true
-    attempt.upcase.chars.each do |char|
-      char_count = attempt.upcase.count(char)
-      grid_count = grid.count(char)
-      parameter = false if char_count > grid_count
-    end
-    parameter
-  end
-
   def english_word?(attempt)
-    url = "https://wagon-dictionary.herokuapp.com/#{attempt}"
-    dictionary_serialized = URI.open(url).read
-    JSON.parse(dictionary_serialized)["found"]
+    uri_response = URI.open("https://dictionary.lewagon.com/#{@user_input}").read
+    parsed_response = JSON.parse(uri_response)
+    @word_found = parsed_response["found"]
   end
 end
